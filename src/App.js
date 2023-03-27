6efb72a012880e111ed06a5dbf57aa26478bcfde
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-03-27 14:50:16 +0530

    Added navigation and link component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/Navigation/navigation.component";
 import {Routes,Route} from 'react-router-dom';
+
+const Shop=()=>{
+  return(
+    <div>
+      <h1>Shop here...</h1>
+    </div>
+  )
+}
+
 const App=()=> {
   return ( 
  <Routes>
-  <Route path='/' element={<Home/>}/>
+  <Route path='/' element={<Navigation/>}>
+    <Route index element={<Home/>}/>
+    <Route path="/Shop" element={<Shop/>}/>
+  </Route>
+  
  </Routes>
   );
 }
